3072026383e9cb32a3446b61d50302d55e3b1865
心猿 <m18438628633@163.com>
2025-04-16 09:22:02 +0800

    修复静态文件配置：在url中添加静态文件路径并创建启动脚本

---
+++ myproject/runserver.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+启动Django开发服务器的辅助脚本
+"""
+import os
+import sys
+import django
+
+if __name__ == "__main__":
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+    django.setup()
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(['manage.py', 'runserver', '127.0.0.1:443']) 
